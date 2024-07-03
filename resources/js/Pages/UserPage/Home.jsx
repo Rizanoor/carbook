9c3f82d93eac756277f4dee267f68c41ce81f622
rizanoor <rizanoorfauzan123@gmail.com>
2024-07-03 07:55:18 +0700

    feat: register component in home page

--- resources/js/Pages/UserPage/Home.jsx
+++ resources/js/Pages/UserPage/Home.jsx
@@ -1,6 +1,9 @@
+import BecomeDriver from '@/Components/molecules/BecomeDriver'
 import Hero from '@/Components/molecules/Hero'
 import MakeYourTrip from '@/Components/molecules/MakeYourTrip'
+import Service from '@/Components/molecules/Service'
 import Welcome from '@/Components/molecules/Welcome'
+import Blog from '@/Components/organisems/Blog'
 import FeatureVehicle from '@/Components/organisems/FeatureVehicle'
 import HomeLayout from '@/Layouts/HomeLayout'
 
@@ -12,6 +15,9 @@ export default function Home() {
             <MakeYourTrip />
             <FeatureVehicle />
             <Welcome />
+            <Service />
+            <BecomeDriver />
+            <Blog />
 
         </HomeLayout>
     </>
